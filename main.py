6221b54cc1fdc54282f8689c43d060452d7abdc5
Maks Golovin <maxxig@gmail.com>
2022-07-18 23:06:40 +0300

    save

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import configparser
-import re
+
 config = configparser.ConfigParser()
 config.sections()
 config.read('config.ini', encoding="utf-8")
@@ -12,20 +12,18 @@ cnfg = config['parameters']
 driver = webdriver.Chrome('D:/_sources/python/mindskills_control/chromedriver_win32/chromedriver.exe')
 driver.get('https://mindskills.online/profile/login/')
 
-#element_login  = driver.find_element(By.XPATH,'//input[@type="email"]')
+
 element_login  = driver.find_element(By.XPATH,"//input[contains(@name,'username')]")
 
-#element_login.send_keys('mgolovin@incap-dev.ru')
 element_login.send_keys(cnfg['login'])
 element_pass  = driver.find_element(By.XPATH,"//input[contains(@name,'password')]")
 element_pass.send_keys(cnfg['password'])
 element_accept = driver.find_element(By.XPATH,'//button[@type="submit"]')
 element_accept.click()
-#element_login  = driver.find_element(By.XPATH,'//div[@id="app"]/div[1]/button[1]/div[1]')
-#element_login.click()
+
 time.sleep(2)
 driver.get('https://mindskills.online/students/task_list/')
-
+# course_task_9676 - alina
 tasks = driver.find_element(By.XPATH,"//div[contains(@class,'course_task_8016')]/ul")
 Danya = dict()
 total_cnt = 0
