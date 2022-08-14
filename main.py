bd2d44ef25e1d8f5966373718b9a4f258fcfc792
Maks Golovin <maxxig@gmail.com>
2022-08-14 20:50:46 +0300

    test sqllite

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-import configparser
-import time
+import configparser,time, sqlite3
+from datetime import date
+
 
 #create webdriver
 options = Options()
@@ -48,13 +49,51 @@ def get_current_student_tasks(login, password, course_id):
         total_cnt += cnt
     res_dict['total_cnt'] = total_cnt
     return res_dict
+def get_count_tasks_today(user_id = 1):
+    conn = sqlite3.connect('mindskills_db.sqlite')
+    current_date = date.today()
+    data = conn.execute('select complited_tasks from users_complited_tasks where date = ? and user_id = ? LIMIT 1', (current_date, user_id))
+    for d in data:
+        res = d
+    conn.close()
+    return res[0]
+def get_previous_total_task_count(user_id):
+    conn = sqlite3.connect('mindskills_db.sqlite')
+    data = conn.execute('select [limit], task_stock from users where id = ? LIMIT 1', [user_id])
+    for d in data:
+        res = d
+    conn.close()
+    return res
+def update_today_stat(user_id, cnt):
+    conn = sqlite3.connect('mindskills_db.sqlite')
+    current_date = date.today()
+    conn.execute('update users_complited_tasks set complited_tasks = ? where user_id = ? and date = ?', (cnt, user_id, current_date))
+    conn.commit()
+    conn.close()
 
 while(1):
-    danya_previous = danya.copy()
+    danya_info = get_previous_total_task_count(1)
+    if not danya:
+        danya_previous['total_cnt'] = danya_info[1]
+    danya_limit = danya_info[0]
+    cnt_complited_tasks_today = get_count_tasks_today(1)
+    # получаем количество решенных за сегодня, если danya_previous нул, если в бд за сегодня тоже нет - 0
     danya = get_current_student_tasks(cnfg['login_danya'], cnfg['password_danya'], cnfg['course_class_id_danya'])
-    if danya.get('total_cnt') != danya_previous.get('total_cnt'):
+
+    if danya.get('total_cnt') == 0:
+        # у дани нет заданий  - сразу отправляем панику - после 8 утра
+        pass
+    if danya.get('total_cnt') > 0 and cnt_complited_tasks_today == 0:
+        # задания есть, ничего не решал - напомнить после 20.00
+        pass
+    if danya.get('total_cnt') < danya_previous.get('total_cnt'):
+        update_today_stat(1, cnt_complited_tasks_today + (danya_previous.get('total_cnt') - danya.get('total_cnt')))
+        # даня порешал, обновим стату
         print(danya_previous)
         print(danya)
+    elif danya.get('total_cnt') > danya_previous.get('total_cnt'):
+        # добавили заданий, не понятно решал ли даня
+        pass
     else:
         print('Даня не решал')
     alina_previous = alina.copy()
